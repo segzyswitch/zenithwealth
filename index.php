ceb3841d4eea8329b4ecc14d25e31e61ff31143b
segzyswitch <segzyswitch99@gmail.com>
2025-11-02 18:55:55 +0100

    chatinex

--- index.php
+++ index.php
@@ -687,17 +687,4 @@ $Controller = new Controller()
 	});
 </script>
 
-<!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-	var _smartsupp = _smartsupp || {};
-	_smartsupp.key = '17e4eb7ee82965d1449125e88d45456a2996c169';
-	window.smartsupp || (function (d) {
-		var s, c, o = smartsupp = function () { o._.push(arguments) }; o._ = [];
-		s = d.getElementsByTagName('script')[0]; c = d.createElement('script');
-		c.type = 'text/javascript'; c.charset = 'utf-8'; c.async = true;
-		c.src = '../www.smartsuppchat.com/loaderd41d.js?'; s.parentNode.insertBefore(c, s);
-	})(document);
-</script>
-<noscript> Powered by <a href=%e2%80%9chttps_/www.smartsupp.html target=“_blank”>Smartsupp</a></noscript>
-
 </html>
