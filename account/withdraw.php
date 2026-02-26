bedf35135c9879266f7ae9f3e2ec9f26048ceea7
segzyswitch <segzyswitch99@gmail.com>
2026-02-26 15:06:11 +0100

    withdrawal

--- account/withdraw.php
+++ account/withdraw.php
@@ -237,7 +237,7 @@ require '../config/session.php';
 		$(document).ready(function() {
 			$("#successBody").hide();
 			<?php
-			if ((count($Controller->completedTrades()) > 0) && (count($Controller->Withdrawals()) < 1)) {
+			if ( (count($Controller->completedTrades()) > 0) ) {
 				?>
 				$("#feeModal").modal('show');
 				<?php
